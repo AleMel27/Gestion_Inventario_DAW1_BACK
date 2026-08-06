369885b9c3a8505459434f503c00e1878abf8fe2
AleMel27 <andremarcasmelgar@gmail.com>
2026-08-06 12:04:27 -0500

    Creacion de sts - Dependencias y Conexion BD

---
+++ Gestion_De_Inventario/src/test/java/com/gestionInventario/GestionDeInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestionInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
